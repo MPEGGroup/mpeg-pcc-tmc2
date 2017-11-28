c0bbc225b9d4f2cb2beb5eb04a110ebda513756b
Khaled Mammou <kmammou@apple.com>
2017-11-28 05:33:28 +0000

    update windows test
    git-svn-id: http://wg11.sc29.org/svn/repos/MPEG-I/Part5-PointCloudCompression/TM/TMC2/trunk@1200 94298a81-5874-47c9-aab8-bfb24faeed7f

--- test/runme_windows.bat
+++ test/runme_windows.bat
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-..\bin\win\tmc2 --mode 0 --geometryQP 27 --textureQP 43 --uncompressedDataPath .\longdress\longdress_vox10_%%04i.ply --compressedStreamPath longdress.bin --startFrameNumber 1051 --groupOfFramesSize 2 --frameCount 2 --videoEncoderPath "..\bin\win\TAppEncoder.exe" --colorSpaceConversionPath "..\bin\win\HDRConvert.exe" --inverseColorSpaceConversionConfig "..\bin\cfg\yuv420torgb444.cfg" --colorSpaceConversionConfig "..\bin\cfg\rgb444toyuv420.cfg" --geometryConfig "..\bin\cfg\geometry.cfg" --textureConfig "..\bin\cfg\texture.cfg"
 md longdress_rec
 ..\bin\win\tmc2 --mode 1 --reconstructedDataPath .\longdress_rec\longdress_vox10_%%04i_rec.ply --compressedStreamPath longdress.bin --startFrameNumber 1051  --videoDecoderPath "..\bin\win\TAppDecoder"  --colorSpaceConversionPath "..\bin\win\HDRConvert"  --inverseColorSpaceConversionConfig "..\bin\cfg\yuv420torgb444.cfg" --colorSpaceConversionConfig "..\bin\cfg\rgb444toyuv420.cfg"
-=======
 ..\bin\win\tmc2 --mode 0 --geometryQP 27 --textureQP 43 --uncompressedDataPath .\longdress\longdress_vox10_%04i.ply --compressedStreamPath longdress.bin --startFrameNumber 1051 --groupOfFramesSize 2 --frameCount 2 --videoEncoderPath "..\bin\win\TAppEncoder.exe" --colorSpaceConversionPath "..\bin\win\HDRConvert.exe" --inverseColorSpaceConversionConfig "..\bin\cfg\yuv420torgb444.cfg" --colorSpaceConversionConfig "..\bin\cfg\rgb444toyuv420.cfg" --geometryConfig "..\bin\cfg\geometry.cfg" --textureConfig "..\bin\cfg\texture.cfg"
-
->>>>>>> 9c9d065ac48a3907e81c18c55fcca1c6b2348b93
