d45c992eea42af736caaa285592b50ed5db26eec
Julien Ricard <julien.ricard@interdigital.com>
2020-05-14 14:58:58 +0200

    add cmake file

---
+++ dependencies/cmake/hdrtools.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 3.10 FATAL_ERROR)
+
+  
+SET( HDRTOOLS_DIR             ${CMAKE_SOURCE_DIR}/dependencies/HDRTools )
+MESSAGE("Clone and build HDRTools libraries: ${HDRTOOLS_DIR}") 
+
+IF( NOT EXISTS "${HDRTOOLS_DIR}/CMakeLists.txt" )   
+  EXECUTE_PROCESS( COMMAND git clone http://gitlab.com/standards/HDRTools.git ${HDRTOOLS_DIR} )  
+ENDIF()
+
+MESSAGE("HDRTOOLS_DIR       : ${HDRTOOLS_DIR}") 
+IF ( NOT EXISTS "${HDRTOOLS_DIR}/common/CMakeLists.txt" )
+  message( FATAL_ERROR "${HDRTOOLS_DIR}/common/CMakeLists.txt not exist")
+ELSE()
+  ADD_SUBDIRECTORY( ${HDRTOOLS_DIR}/common ) 
+ENDIF()
